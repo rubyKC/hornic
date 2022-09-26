6512ffdfc4c21d1ffb270eef17fc21aad20a6d31
看城 关 <2868303155@qq.com>
2022-09-26 18:50:40 +0800

    add loop and structdef nothing more

--- hornic.h
+++ hornic.h
@@ -11,12 +11,20 @@ typedef unsigned int u16;         //same as short
 typedef unsigned long int u32;
 typedef float f32;              //same as double
 
+//just for fun
+#define loop for(;;)
+#define structdef typedef struct
+/*
+structdef type{
+
+}asd
+*/
 
 //always be used for some not bit-addressable stuff
 #define bset1(addr,bit_pos) addt &= ~(1<<bit_pos)
 #define bset0(addr,bit_pos) addr |=  (1<<bit_pos)
 
-//scroll bit pattern
+//scroll bit pattern (need intrins.h)
 #define scroll_bit(size, orientation, val, turn_val) \
     _size##ro##orientation_(val, turn_val)
 
